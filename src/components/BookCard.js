74bc054405358da7fbdff627cf47616352e10833
vetleenb <122876020+vetleenb@users.noreply.github.com>
2026-03-11 15:24:10 +0100

    Update BookCard.js

--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -2,14 +2,23 @@ import React from 'react';
 
 const BookCard = ({ book }) => {
   return (
-    <div className="book">
-      <h2>{book.title}</h2>
-      <p>Forfatter: {book.author_name ? book.author_name.join(', ') : 'Ian Fleming'}</p>
-      <p>År: {book.first_publish_year}</p>
-  {book.cover_i && <img src={`https://covers.openlibrary.org/b/id/${book.cover_i}-L.jpg`} alt="Book Cover" />}
+    <div className="book-card">
+      {book.cover_i && (
+        <img 
+          src={`https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`} 
+          alt={book.title} 
+          className="book-cover"
+        />
+      )}
+      <div className="book-info">
+        <h2>{book.title}</h2>
+        <p>Forfatter: {book.author_name ? book.author_name.join(', ') : 'Ukjent'}</p>
+        <p>År: {book.first_publish_year || 'Ukjent'}</p>
+      </div>
     </div>
   );
 };
+
 export default BookCard;
 
 
